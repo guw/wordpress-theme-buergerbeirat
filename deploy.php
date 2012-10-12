4e8dd67cb65db21ddc795050a2eb61adb2bafadd
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2012-10-12 10:01:45 +0200

    add deploy script and project settings
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

---
+++ deploy.php
@@ -0,0 +1,52 @@
+<?php
+
+  // The repo/site to deploy
+  /*
+  $clientIp = '';
+  if ( isset($_SERVER["REMOTE_ADDR"]) )    { 
+    $clientIp = '' . $_SERVER["REMOTE_ADDR"]; 
+  } else if ( isset($_SERVER["HTTP_X_FORWARDED_FOR"]) )    { 
+    $clientIp = '' . $_SERVER["HTTP_X_FORWARDED_FOR"]; 
+  } else if ( isset($_SERVER["HTTP_CLIENT_IP"]) )    { 
+    $clientIp = '' . $_SERVER["HTTP_CLIENT_IP"]; 
+  } 
+  */
+
+  // The commands
+  $commands = array(
+    'echo $PWD',
+    'whoami',
+    'git reset --hard HEAD',
+    'git pull',
+    'git status',
+    'git submodule sync',
+    'git submodule update',
+    'git submodule status',
+  );
+
+  // Run the commands for output
+  $output = '';
+  foreach($commands AS $command){
+    // Run it
+    $tmp = shell_exec($command);
+    // Output
+    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+    $output .= htmlentities(trim($tmp)) . "\n";
+  }
+
+  // Make it pretty for manual user access (and why not?)
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <title>Git Deployment Script</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<pre>
+<?php echo $output; ?>
+</pre>
+</body>
+</html>
