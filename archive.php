5cefc377c0d34191855bd2d6f1adcaf00d3636d8
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2012-12-03 11:33:20 +0100

    Lokalisierung

--- archive.php
+++ archive.php
@@ -1,4 +1,4 @@
-<?php get_header(); ?>
+﻿<?php get_header(); ?>
 			
 			<div id="content" class="clearfix row-fluid">
 			
@@ -7,27 +7,27 @@
 					<div class="page-header">
 					<?php if (is_category()) { ?>
 						<h1 class="archive_title h2">
-							<span><?php _e("Posts Categorized:", "bonestheme"); ?></span> <?php single_cat_title(); ?>
+							<?php single_cat_title(); ?>
 						</h1>
 					<?php } elseif (is_tag()) { ?> 
 						<h1 class="archive_title h2">
-							<span><?php _e("Posts Tagged:", "bonestheme"); ?></span> <?php single_tag_title(); ?>
+							<?php single_tag_title(); ?>
 						</h1>
 					<?php } elseif (is_author()) { ?>
 						<h1 class="archive_title h2">
-							<span><?php _e("Posts By:", "bonestheme"); ?></span> <?php get_the_author_meta('display_name'); ?>
+							<span><?php _e("Beiträge von:", "bonestheme"); ?></span> <?php get_the_author_meta('display_name'); ?>
 						</h1>
 					<?php } elseif (is_day()) { ?>
 						<h1 class="archive_title h2">
-							<span><?php _e("Daily Archives:", "bonestheme"); ?></span> <?php the_time('l, F j, Y'); ?>
+							<?php the_time('l, F j, Y'); ?>
 						</h1>
 					<?php } elseif (is_month()) { ?>
 					    <h1 class="archive_title h2">
-					    	<span><?php _e("Monthly Archives:", "bonestheme"); ?>:</span> <?php the_time('F Y'); ?>
+					    	<?php the_time('F Y'); ?>
 					    </h1>
 					<?php } elseif (is_year()) { ?>
 					    <h1 class="archive_title h2">
-					    	<span><?php _e("Yearly Archives:", "bonestheme"); ?>:</span> <?php the_time('Y'); ?>
+					    	<?php the_time('Y'); ?>
 					    </h1>
 					<?php } ?>
 					</div>
